b811e963a81b8f80c8643183b35a698403a65053
Akulov Pavel <akulov_pavel88@mail.ru>
2017-01-03 22:30:29 +0200

    utf +1

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+var db = require('../db');
+db.connect();
+
 var User = require('./user');
 
 function run(){
     var vasya = new User("Вася");
     var petya = new User("Петя");
     vasya.hello(petya);
+    console.log(db.getPhrase("Run successful"))
 }
 if (module.parent){
     exports.run = run;
